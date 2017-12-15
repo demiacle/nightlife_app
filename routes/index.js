0b840f41dd6ac34764438b199f395759b23ed9e2
demiacle <demiacle@gmail.com>
2017-12-14 22:01:30 -0800

    Added styles and switched to graphql

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch')
-var passport = require('passport')
+var passport = require('passport');
+var graphql = require('graphql-request')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,20 +15,35 @@ router.get('/auth/twitter/callback', passport.authenticate('twitter', {failureRe
 })
 
 router.get('/search/:query', function(req, res ){
-  console.log( process.env.YELP_KEY)
- fetch('https://api.yelp.com/v3/businesses/search?location='+ encodeURIComponent( req.params.query ) + '&categories=bars', {
-        method: 'get',
-        headers: {
-          Authorization: 'Bearer ' + process.env.YELP_KEY
+  console.log('searching: ' + req.params.query)
+  var client = new graphql.GraphQLClient(
+    'https://api.yelp.com/v3/graphql', 
+    { 
+      headers: {
+        'Authorization': 'Bearer ' + process.env.YELP_KEY
+      }
+    }
+  )
+  var query = `{
+    search(location: "${req.params.query}", categories: "bars") {
+      business{
+        name
+        id
+        url
+        location {
+          formatted_address
         }
-      }).then(function(response) {
-        return response.json();
-      }).then(function(data) {
-
-        console.log(data)
-        res.json(data)
-      }).catch(function(err) {
-        alert('Error requesting yelp api')
-      });
+        photos
+        reviews {
+          text
+        }
+      }
+    }
+  }`
+  console.log(query)
+  client.request(query).then( data =>{ 
+      console.log(data) 
+      res.json(data)
+    }).catch(err => console.log(err))
 })
 module.exports = router;
